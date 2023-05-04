1ce5bc40734951033ae037d800374876a2754f6a
Werner <turbotimoh@gmail.com>
2023-05-04 10:28:08 +0545

    feat: create `index`, `new` and `create` methods for `KittensController`

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,40 @@
 class KittensController < ApplicationController
+  
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = "Kitten created!"
+      redirect_to kittens_path
+    else
+      flash[:error] = @kitten.errors.full_messages.first
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
