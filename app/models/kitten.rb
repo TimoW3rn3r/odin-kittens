4f9fb9bd5492f2bcbb0c651aae3c440973c2b718
Werner <turbotimoh@gmail.com>
2023-05-04 13:08:13 +0545

    feat: specify attributes to return for json request

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -3,4 +3,8 @@ class Kitten < ApplicationRecord
   validates :age, inclusion: { in: 1..100, message: "is invalid" }
   validates :cuteness, presence: true
   validates :softness, presence: true
+
+  def as_json(options={})
+    super({ only: [:name, :age, :cuteness, :softness] }.merge(options))
+  end
 end
