41d704e9433365f8bb7bcdd88be7f8be2e0252ff
Werner <turbotimoh@gmail.com>
2023-05-04 11:13:10 +0545

    feat: add validations to `Kitten` model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :age, inclusion: { in: 1..100, message: "is invalid" }
+  validates :cuteness, presence: true
+  validates :softness, presence: true
 end
